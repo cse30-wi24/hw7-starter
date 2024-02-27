a308605c7ea6e26e205bff24d4e0a772fd95726c
Yuchen Jing <y5jing@ucsd.edu>
2024-02-26 18:07:30 -0800

    Initial commit: add starter code

---
+++ cse30liferevisited.h
@@ -0,0 +1,32 @@
+#ifndef CSE30LIFE_H
+#define CSE30LIFE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef uint8_t Cell;
+
+typedef struct {
+	Cell *buf1; /* pointer to first life board buffer */
+	Cell *buf2; /* pointer to ansecond life board buffer */
+	size_t nrows; /* number of rows in the life board */
+	size_t ncols; /* number of cols in the life board */
+	Cell *buffer; /* pointer to the current life board's buffer */
+	Cell *next_buffer; /* pointer to the next iteration's board buffer */
+	unsigned int gen; /* generation number */
+} Board;
+
+void delete_board(Board **board);
+void clear_board(Board *board);
+void swap_buffers(Board *board);
+void sim_step(Board *board, unsigned int steps);
+void set_sim(Board *board, void (*sim)(Board *board, unsigned int steps));
+size_t get_index(size_t ncols, size_t row, size_t col);
+
+size_t mod(int x, size_t N);
+
+Board *create_board(const char *filename);
+void sim_loop(Board *board, unsigned int steps);
+void do_row(Cell *dest, Cell *src, size_t row, size_t rows, size_t cols);
+
+#endif
